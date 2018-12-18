8e27ca7dc52e5b9b96f71fcc36763ff3c19bae07
ngundotra <noah@gundotra.org>
2018-12-17 23:21:19 -0800

    * untested gdal-install script, but should work. might need system restart, which I had to do. I assumed it was bc of the weird C/C++ headers not being found properly

--- albu/gdal-install.sh
+++ albu/gdal-install.sh
@@ -4,6 +4,9 @@ sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
 sudo apt-get update
 sudo apt-get install gdal-bin
 sudo apt-get install libgdal-dev
+sudo apt install python3-gdal python-gdal
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
-pip install GDAL
+# Here's what worked for me on Ubuntu 16.04
+sudo python -m pip install GDAL==$(gdal-config --version) --global-option=build_ext --global-option="-I/usr/include/gdal -L/usr/local/lib -lgdal"
+
