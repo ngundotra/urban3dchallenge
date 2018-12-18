afbfd36b1365644c827981cbefa85e7d57e630c3
ngundotra <noah@gundotra.org>
2018-12-18 00:32:26 -0800

    * just create a new conda env, RIP dependency management

--- albu/gdal-install.sh
+++ albu/gdal-install.sh
@@ -7,6 +7,5 @@ sudo apt-get install libgdal-dev
 sudo apt install python3-gdal python-gdal
 export CPLUS_INCLUDE_PATH=/usr/include/gdal
 export C_INCLUDE_PATH=/usr/include/gdal
-# Here's what worked for me on Ubuntu 16.04
-sudo python -m pip install GDAL==$(gdal-config --version) --global-option=build_ext --global-option="-I/usr/include/gdal -L/usr/local/lib -lgdal"
-
+# After this, I just created I new conda env, reinstalled everything, then just did
+conda install gdal
